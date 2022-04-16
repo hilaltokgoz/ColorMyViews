520e79b797b89c05f35e0ca414269541a9821bfd
Hilal <hilaltokgoz98@gmail.com>
2022-04-16 13:29:48 +0300

    layout ended

--- app/src/main/java/com/colormyviews/MainActivity.kt
+++ app/src/main/java/com/colormyviews/MainActivity.kt
@@ -1,25 +1,47 @@
 package com.colormyviews
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListener()
     }
 
-//Renkleri değiştirmek için
-    private fun makeColored(view: View){
-        when(view.id){
-         R.id.boxOneText->view.setBackgroundColor(Color.DKGRAY)
-         R.id.boxTwoText->view.setBackgroundColor(Color.GRAY)
-         R.id.boxThreeText->view.setBackgroundColor(Color.BLUE)
-         R.id.boxFourText->view.setBackgroundColor(Color.MAGENTA)
-         R.id.boxFiveText->view.setBackgroundColor(Color.BLUE)
-            else->view.setBackgroundColor(Color.LTGRAY)
+    //Renkleri değiştirmek için
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.boxTwoText -> view.setBackgroundColor(Color.GRAY)
+            R.id.boxThreeText -> view.setBackgroundColor(Color.BLUE)
+            R.id.boxFourText -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.boxFiveText -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
+
+    fun setListener() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOneText,
+            binding.boxTwoText,
+            binding.boxThreeText,
+            binding.boxFourText,
+            binding.boxFiveText,
+            binding.constraintLayout
+        )
+        for (item in clickableViews){
+            item.setOnClickListener { makeColored(it) }
+        }
+
+    }
+
+
 }
